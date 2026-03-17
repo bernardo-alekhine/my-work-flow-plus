4dbbe4b64926048f76a64a238085fb2a6f7e2f97
bernardo-alekhine <alekhinebernardo@gmail.com>
2026-03-17 01:07:03 +0000

    feat: fix dockerfile and gunicorn setup

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -7,4 +7,4 @@ workers = int(os.getenv("WEB_CONCURRENCY", multiprocessing.cpu_count() * 2 + 1))
 timeout = 120
 accesslog = "-"
 errorlog = "-"
-wsgi_app = os.path.join(os.path.dirname(__file__), "..", "src", "config.wsgi:application")
+wsgi_app = "src.config.wsgi:application" 
